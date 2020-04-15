08bb27338d5922ef4c1c40bf32aab46f84041fbb
mateoox600 <matboi862@gmail.com>
2020-04-15 12:26:05 +0200

    Added comment

--- js/index.js
+++ js/index.js
@@ -1,23 +1,33 @@
 
+// importing html elements
 var position_label = document.getElementById("location-name");
-
 var go_city_button = document.getElementById("go-city-button");
 var backpack_button = document.getElementById("backpack-button");
 var tavern_button = document.getElementById("tavern-button");
 
 function init(){
-    go_city_button.onclick = function(){
-        position_label.innerHTML = "City";
-        go_city_button.style.visibility = "hidden";
-    };
-    backpack_button.onclick = function(){
-        position_label.innerHTML = "BackPack";
-        go_city_button.style.visibility = "visible";
-    };
-    tavern_button.onclick = function(){
-        position_label.innerHTML = "Tavern";
-        go_city_button.style.visibility = "visible";
-    };
+    // setting up the click event for the button
+    go_city_button.onclick = function(){ go_city(); };
+    backpack_button.onclick = function(){ go_backpack(); };
+    tavern_button.onclick = function(){ go_tavern(); };
+}
+
+// updating html element to go to the city
+function go_city(){
+    position_label.innerHTML = "City";
+    go_city_button.style.visibility = "hidden";
+}
+
+// updating html element to go to the backpack
+function go_backpack(){
+    position_label.innerHTML = "BackPack";
+    go_city_button.style.visibility = "visible";
+}
+
+// updating html element to go to the tavern
+function go_tavern(){
+    position_label.innerHTML = "Tavern";
+    go_city_button.style.visibility = "visible";
 }
 
 init();
